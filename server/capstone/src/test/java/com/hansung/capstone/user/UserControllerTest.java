de17f6426c780e10f16f5f666ca488f6db5a5769
devch96 <chjung1006@gmail.com>
2023-01-27 11:58:22 +0900

    testDB connection completed

--- server/capstone/src/test/java/com/hansung/capstone/user/UserControllerTest.java
+++ server/capstone/src/test/java/com/hansung/capstone/user/UserControllerTest.java
@@ -2,6 +2,10 @@ package com.hansung.capstone.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hansung.capstone.SecurityConfig;
+import com.hansung.capstone.user.AppUser;
+import com.hansung.capstone.user.UserCreateForm;
+import com.hansung.capstone.user.UserRepository;
+import com.hansung.capstone.user.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,6 +18,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -52,6 +57,23 @@ class UserControllerTest {
                 .addFilters(new CharacterEncodingFilter("UTF-8", true))  // 한글 깨짐 처리
                 .build();
     }
+
+    @Test
+    @DisplayName("Post register test - /login/register")
+    void registerTest() throws Exception{
+        UserCreateForm user = UserCreateForm.builder()
+                .email("hoon@hoon.com")
+                .password1("1234")
+                .password2("1234")
+                .username("hoon")
+                .build();
+
+        mockMvc.perform(post("http://localhost:8080/login/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
     @Test
     @DisplayName("Get check")
     void getCheck() throws Exception {
@@ -60,81 +82,22 @@ class UserControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk());
     }
-    @Test
-    void testAdd() {
-        assertEquals(42, Integer.sum(19, 23));
-    }
 
     @Test
     @DisplayName("Post login check - /login/check")
     void loginCheckTest() throws Exception {
         AppUser user1 = AppUser.builder()
-                .username("test")
-                .password("test").build();
-
-        userService.check(user1);
+                .username("hoon")
+                .password("1234").build();
 
         String content = objectMapper.writeValueAsString(user1);
-
-        System.out.println(objectMapper.writeValueAsString(user1));
         mockMvc.perform(post("/login/check")
                         .content(content)
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
                         .accept(MediaType.APPLICATION_JSON_VALUE))
                 .andDo(print())
-//                .andExpect(status().isOk())
+                .andExpect(status().isOk())
                 .andExpect(content().string("good"));
 
-    }
-
-    @Test
-    @DisplayName("Post register test - /login/register")
-    void registerTest() throws Exception{
-        UserCreateForm user = UserCreateForm.builder()
-                .email("test")
-                .password1("1234")
-                .password2("1234")
-                .username("ho3on")
-                .build();
-
-        mockMvc.perform(post("http://localhost:8080/login/register")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(user))
-                        .with(csrf()))
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    @DisplayName("real test")
-    void testTest() throws Exception {
-        UserCreateForm user = UserCreateForm.builder()
-                .email("hi@hi")
-                .password1("123")
-                .password2("123")
-                .username("hi")
-                .build();
-
-        mockMvc.perform(post("/login/hi")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(user)))
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    @DisplayName("post check")
-    void postCheck() throws Exception {
-        AppUser user = AppUser.builder()
-                .username("test")
-                .password("test").build();
-
-        mockMvc.perform(post("/login/blog")
-                        .content(objectMapper.writeValueAsString(user))
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .accept(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andExpect(content().string("test의 블로그입니다."))
-                .andDo(print());
     }
 }
